54fd71b4f3733a5377e488df534a0afd5bdba379
eric sartor <ericdavidsartor@live.ca>
2024-11-24 22:20:35 -0500

    make a super menu page for the main site

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,28 @@
 // import { startGame } from "./practice2";
 import { startGame } from "./bat-game/main";
 
-const startGameButton = document.createElement('button');
-startGameButton.textContent = 'Start Game';
-startGameButton.addEventListener('click', () => {
-  startGameButton.parentElement?.removeChild(startGameButton);
-  startGame();
+const games: {
+  name: string;
+  credits: string;
+  start: Function;
+}[] = [
+  {
+    name: 'Bat Game',
+    credits: 'Programming / Game Design by Eric Sartor, Art / Game Design by Bridget Fae',
+    start: startGame,
+  }
+];
+
+document.body.innerHTML = '<div>We are a lil hobby game studio!</div>';
+
+games.forEach((game) => {
+  const div = document.createElement('div');
+  div.innerHTML = `
+    <button style="margin-right: 12px;">Play</button>${game.name} - ${game.credits}
+  `;
+  div.children[0].addEventListener('click', () => {
+    document.body.innerHTML = '';
+    game.start();
+  });
+  document.body.appendChild(div);
 });
-document.body.appendChild(startGameButton);
