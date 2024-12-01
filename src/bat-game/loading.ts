66b36aba73946520fa936426448a07bb10317f02
eric sartor <ericdavidsartor@live.ca>
2024-12-01 11:24:05 -0500

    add loading percent

--- src/bat-game/loading.ts
+++ src/bat-game/loading.ts
@@ -29,11 +29,23 @@ export const setStartingScene = (scene: Phaser.Scene) => {
 // Always the first scene loaded
 export class LoadingScene extends Phaser.Scene {
 
+    loadingText: Phaser.GameObjects.Text | null = null;
+    loadingPercent = 0;
+
     constructor() {
         super('Loading');
     }
 
     preload() {
+        // Set up loading progress logic
+        this.load.on('progress', (percent: number) => {
+            this.loadingPercent = Math.round(percent * 100);
+        });
+        this.load.on('complete', () => {
+            this.scene.start('Testing');
+        });
+
+        // Load assets
         this.load.baseURL = '/bat-game'
         loadingFuncs.forEach((func) => {
             func(this);
@@ -43,11 +55,21 @@ export class LoadingScene extends Phaser.Scene {
     create() {
         if (startingScene === null) throw Error('starting scene was not set');
 
+        // Create loading text
+        const text = this.add.text(0, 0, 'Loading 0%', { color: '#000000' });
+        this.loadingText = text;
+        this.cameras.main.centerOn(0, 0);
+
+        // Run creation functions
         createFuncs.forEach((func) => {
             func(this);
         });
+    }
 
-        this.scene.start('Testing');
+    update(): void {
+        if (this.loadingText) {
+            this.loadingText.setText(`Loading ${this.loadingPercent}%`)
+        }
     }
 
 }
