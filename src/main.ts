ca68d966b8edb2bfbb7b0a7a1debfce187401db7
Eric Sartor <ericdavidsartor@live.ca>
2025-02-21 16:21:34 -0500

    make website look tolerable

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,49 @@
 const games: {
   name: string;
-  credits: string;
+  description: string;
+  credits: string[];
   path: string;
 }[] = [
   {
     name: 'Babs the Bat',
-    credits: 'Programming / Game Design by Eric Sartor, Art / Sound / Game Design by Bridget Fae, Music / Sound / Game Design by Ryan Ulch',
+    description: 'Fly around eating flies for as long as you can, dodging falling sheep, spitting fish, and a rude frog.',
+    credits: [
+      'Eric Sartor: Programming / Game Design',
+      'Bridget Fae: Art / Sound / Game Design',
+      'Ryan Ulch: Music / Sound / Game Design',
+    ],
     path: '/babs/build.html',
   }
 ];
 
-document.body.innerHTML = '<div>We are a lil hobby game studio!</div>';
+document.body.innerHTML = `
+  <h1>Smoking Panda Games</h1>
+  <p>Games made by Eric Sartor, with the help and support of some other pretty cool people!</p>
+  <div id="game-list"></div>
+`;
 
+const gameListEl: HTMLElement = document.getElementById('game-list')!;
 games.forEach((game) => {
   const div = document.createElement('div');
+  div.style.padding = "12px";
+  div.style.margin = "12px 0";
+  div.style.border = "1px solid black";
+  div.style.borderRadius = "6px";
   div.innerHTML = `
-    <button style="margin-right: 12px;">Play</button>${game.name} - ${game.credits}
+    <h2>${game.name}</h2>
+    <main>
+      <p>${game.description}</p>
+      <p>
+        <button class="play">Play</button>
+      </p>
+      <h3>Credits</h3>
+      <p>
+        ${game.credits.join('<br>')}
+      </p>
+    <main>
   `;
-  div.children[0].addEventListener('click', () => {
+  div.querySelector('button.play')!.addEventListener('click', () => {
     location.assign(game.path);
   });
-  document.body.appendChild(div);
+  gameListEl.appendChild(div);
 });
